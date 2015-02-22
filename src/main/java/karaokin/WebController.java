7ba967e7db08eafd7ec2a9f764f189d4d645a6e3
Ashley Barton <barton.ash@gmail.com>
2015-02-21 16:29:11 -0800

    temporarily commenting out db settings

--- src/main/java/karaokin/WebController.java
+++ src/main/java/karaokin/WebController.java
@@ -1,10 +1,18 @@
 package karaokin;
 
+import java.net.UnknownHostException;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
 import com.google.gson.*;
+import com.mongodb.DB;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import com.mongodb.MongoException;
 
 /**
  * This is the controller used by Spring framework.
@@ -16,6 +24,17 @@ import com.google.gson.*;
 
 @RestController
 public class WebController {
+	
+	@SuppressWarnings("deprecation")
+	@Bean
+    public static DB getDb() throws UnknownHostException, MongoException {
+        String uri="mongodb://chantal:hackathon@ds047591.mongolab.com:47591/jukebox";
+        MongoClientURI mongoClientURI=new MongoClientURI(uri);
+        MongoClient mongoClient=new MongoClient(mongoClientURI);
+        DB db=mongoClient.getDB(mongoClientURI.getDatabase());
+        db.authenticate(mongoClientURI.getUsername(),mongoClientURI.getPassword());
+        return db;
+    }
 
     /**
      * This is a simple example of how the HTTP API works.
@@ -32,14 +51,15 @@ public class WebController {
         return "OK";
     }
     
-    @RequestMapping(value = "/data/{song_num}/", method = RequestMethod.GET)
+    /*@RequestMapping(value = "/data/{song_num}/", method = RequestMethod.GET)
     String getSongData(
     		@PathVariable("song_num") int song_num){
     	JsonObject song_ID = new JsonObject();
     	song_ID.addProperty("songID", song_num);
-    	songDB.songid.find({id: 1});
+    	DB songdb = getDb();
+    	String songInfo = songdb.songid.find({id: 1});
     	Gson gson = new GsonBuilder().setPrettyPrinting().serializeNulls().setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE).create();
     	return gson.toJson(song_ID);
     }		
-
+*/
 }
